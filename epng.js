60170d9d32fe026e1d0940013ac98805ee79a4b8
Nildo Vieira Pontes <nildo.pontes@bol.com.br>
2025-02-22 15:31:35 -0300

    Melhora set32bit()

--- epng.js
+++ epng.js
@@ -119,8 +119,8 @@ class ePNG {
    dump32bit(a, b, c, d){
       return Number((BigInt(a) << 24n) + (BigInt(b) << 16n) + (BigInt(c) << 8n) + BigInt(d));
    }
-   set32bit(value){
-     return new Uint8Array([(value >> 24) & 255, (value >> 16) & 255, (value >>  8) & 255, value & 255]);
+   set32bit(v){
+     return new Uint8Array([v >> 24 & 255, v >> 16 & 255, v >> 8 & 255, v & 255]);
    }
    makeCRCTable(){
       let c, a = [];
